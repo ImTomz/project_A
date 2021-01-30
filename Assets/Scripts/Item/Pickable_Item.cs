fd5d821fecb08a547edececbee8d130b3ac7e99e
Tomas Boda <tomasboda77@gmail.com>
2021-01-30 19:08:30 +0100

    basic inventory setup

--- Assets/Scripts/Item/Pickable_Item.cs
+++ Assets/Scripts/Item/Pickable_Item.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class Pickable_Item : MonoBehaviour
 {
+
+    private Inventory inventory;
+    public GameObject itemButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Player")
         {
-            print("item picked");
-            Destroy(gameObject);
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+                print("Inventory full");
+            }
         }
     }
 }
