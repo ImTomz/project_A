6704b18a81a7b4c44a46c086a6dc3b4a3e6e4559
Tomas Boda <tomasboda77@gmail.com>
2021-02-01 19:46:17 +0100

    experimental build

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float moveSpeed;
+    public float moveSpeed = 1;
+    public float jumpForce = 1;
 
     Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+       movement.x = Input.GetAxisRaw("Horizontal");
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        transform.position += new Vector3(movement.x,0,0) * Time.fixedDeltaTime * moveSpeed;
+
+        if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
     }
 }
